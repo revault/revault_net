92e923dfd22f8d0e481d35164affb0f44e9ebb02
JSwambo <jake.t.swambo@hotmail.co.uk>
2021-02-09 13:07:06 +0000

    noise: replace NoisePubKey and NoisePrivKey with sodiumoxide's PublicKey and SecretKey

--- fuzz/fuzz_targets/encrypt_roundtrip.rs
+++ fuzz/fuzz_targets/encrypt_roundtrip.rs
@@ -3,20 +3,20 @@ use libfuzzer_sys::fuzz_target;
 use revault_net::noise::*;
 use std::convert::TryInto;
 
-const INIT_PRIVKEY: NoisePrivKey = NoisePrivKey([
+const INIT_PRIVKEY: SecretKey = SecretKey([
     16, 85, 69, 127, 155, 247, 36, 200, 184, 156, 230, 255, 16, 125, 113, 4, 95, 78, 76, 188, 58,
     21, 55, 146, 195, 160, 199, 82, 41, 109, 199, 81,
 ]);
-const INIT_PUBKEY: NoisePubKey = NoisePubKey([
+const INIT_PUBKEY: PublicKey = PublicKey([
     10, 12, 215, 103, 252, 231, 156, 109, 147, 53, 1, 147, 42, 240, 233, 242, 164, 67, 0, 81, 86,
     180, 233, 168, 75, 29, 216, 242, 15, 186, 225, 102,
 ]);
 
-const RESP_PRIVKEY: NoisePrivKey = NoisePrivKey([
+const RESP_PRIVKEY: SecretKey = SecretKey([
     96, 240, 118, 161, 68, 25, 19, 15, 12, 238, 118, 69, 95, 52, 3, 130, 2, 107, 15, 25, 135, 234,
     72, 36, 67, 124, 36, 228, 203, 101, 122, 110,
 ]);
-const RESP_PUBKEY: NoisePubKey = NoisePubKey([
+const RESP_PUBKEY: PublicKey = PublicKey([
     19, 103, 106, 15, 169, 190, 254, 15, 187, 105, 61, 163, 152, 251, 238, 139, 253, 160, 165, 89,
     108, 67, 194, 161, 42, 72, 15, 38, 109, 193, 45, 125,
 ]);
@@ -43,7 +43,7 @@ fn kk_roundtrip(client_channel: &mut KKChannel, server_channel: &mut KKChannel, 
     let length = server_channel.decrypt_header(&header).unwrap();
     assert_eq!(body.0.len(), length as usize);
     let plaintext = server_channel.decrypt_message(&body).unwrap();
-    assert_eq!(&plaintext, data);
+    assert_eq!(plaintext, data);
 
     // Server --> Client
     let cypher = server_channel.encrypt_message(data).unwrap();
@@ -53,7 +53,7 @@ fn kk_roundtrip(client_channel: &mut KKChannel, server_channel: &mut KKChannel, 
     let length = client_channel.decrypt_header(&header).unwrap();
     assert_eq!(body.0.len(), length as usize);
     let plaintext = client_channel.decrypt_message(&body).unwrap();
-    assert_eq!(&plaintext, data);
+    assert_eq!(plaintext, data);
 }
 
 fuzz_target!(|data: &[u8]| {
--- fuzz/fuzz_targets/transport.rs
+++ fuzz/fuzz_targets/transport.rs
@@ -1,22 +1,23 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
-use revault_net::{noise::*, transport::*};
+use revault_net::noise::{PublicKey, SecretKey};
+use revault_net::transport::*;
 use std::{net::TcpListener, thread};
 
-const INIT_PRIVKEY: NoisePrivKey = NoisePrivKey([
+const INIT_PRIVKEY: SecretKey = SecretKey([
     16, 85, 69, 127, 155, 247, 36, 200, 184, 156, 230, 255, 16, 125, 113, 4, 95, 78, 76, 188, 58,
     21, 55, 146, 195, 160, 199, 82, 41, 109, 199, 81,
 ]);
-const INIT_PUBKEY: NoisePubKey = NoisePubKey([
+const INIT_PUBKEY: PublicKey = PublicKey([
     10, 12, 215, 103, 252, 231, 156, 109, 147, 53, 1, 147, 42, 240, 233, 242, 164, 67, 0, 81, 86,
     180, 233, 168, 75, 29, 216, 242, 15, 186, 225, 102,
 ]);
 
-const RESP_PRIVKEY: NoisePrivKey = NoisePrivKey([
+const RESP_PRIVKEY: SecretKey = SecretKey([
     96, 240, 118, 161, 68, 25, 19, 15, 12, 238, 118, 69, 95, 52, 3, 130, 2, 107, 15, 25, 135, 234,
     72, 36, 67, 124, 36, 228, 203, 101, 122, 110,
 ]);
-const RESP_PUBKEY: NoisePubKey = NoisePubKey([
+const RESP_PUBKEY: PublicKey = PublicKey([
     19, 103, 106, 15, 169, 190, 254, 15, 187, 105, 61, 163, 152, 251, 238, 139, 253, 160, 165, 89,
     108, 67, 194, 161, 42, 72, 15, 38, 109, 193, 45, 125,
 ]);
@@ -34,7 +35,7 @@ fn kk_client_server(data: &[u8]) {
 
     let mut serv_transport = KKTransport::accept(&listener, &RESP_PRIVKEY, &[INIT_PUBKEY]).unwrap();
     if let Ok(msg) = serv_transport.read() {
-        assert_eq!(&msg, data);
+        assert_eq!(msg, data);
     }
 }
 
